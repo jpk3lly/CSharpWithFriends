81673a06e262fa6ab405088749375622a9e5f9aa
John-Paul Kelly <jpk3lly@hotmail.com>
2022-10-01 23:33:15 +0100

    Aligning Branches

---
+++ CSharpWithFriends/OutputMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWithFriends
+{
+    public class OutputMessages
+    {
+        public string welcome = "Welcome to C# with freinds - enjoy :)";
+
+        public string welcomeMessage()
+        {
+            return welcome;
+        }
+    }
+}
---
+++ CSharpWithFriends/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpWithFriends 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputMessages outputMessages = new OutputMessages();
+            Console.WriteLine(outputMessages.welcome);
+        }
+    }
+}
